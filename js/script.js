7a45fe76606fe0fbc02dbf2170080a331b76cd9b
dmitriyyk <136074355+dmitriyyk@users.noreply.github.com>
2024-04-26 18:21:05 +0700

    good

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const scrollContainer = document.querySelector('.page');
+const scrollItems = document.querySelectorAll('section');
+
+window.addEventListener('load', () => {
+  scrollItems.forEach((item) => {
+    item.classList.add('show-animate');
+  });
+});
+
+scrollContainer.addEventListener('scroll', () => {
+  scrollItems.forEach((item) => {
+    const isInViewport = item.getBoundingClientRect().top >= 0 && item.getBoundingClientRect().top <= window.innerHeight - 100;
+
+    if (isInViewport) {
+      item.classList.add('show-animate');
+    } else {
+      item.classList.remove('show-animate');
+    }
+  });
+});
